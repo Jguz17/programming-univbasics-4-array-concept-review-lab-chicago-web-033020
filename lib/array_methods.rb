51f8b93535f4d2e06c90d539014a8888e740e00d
Johnathan  Guzman <johnathanguzman17@gmail.com>
2020-03-11 01:27:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,8 +5,9 @@ def find_element_index(array, value_to_find)
     if array[count] == value_to_find
       return count
     end
+    count += 1
   end
-  count += 1
+  nil
 end
 
 def find_max_value(array)
